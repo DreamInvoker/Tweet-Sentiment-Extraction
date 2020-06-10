3e8ffed82c158717c9b152935d59e4103d237eea
zengshuang <631710518@qq.com>
2020-06-10 15:40:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 推特情感短语抽取
+
+# 训练
+```shell script
+./run.sh GPU_id
+```
+
+# 测试
+
+-将得到的submission.csv提交至[kaggle比赛平台](https://www.kaggle.com/c/tweet-sentiment-extraction/submissions)
---
+++ run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+export CUDA_VISIBLE_DEVICES=$1
+nohup python3.6 -u main.py \
+--model roberta-base \
+--num_warmup_steps 100 \
+	> train.log 2>&1 &
+
+#--model roberta-large \
